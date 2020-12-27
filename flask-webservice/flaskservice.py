d77d04bc018b6276b8f5ea81dbb71c803f1b3b13
lorentinarp <loreto.romero@sansano.usm.cl>
2020-12-27 12:41:39 -0300

    intento de imagenes 2

--- flask-webservice/flaskservice.py
+++ flask-webservice/flaskservice.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, render_template, redirect, url_for, send_from_directory
+from flask import Flask, request, render_template, redirect, url_for
 import os
 
-UPLOAD_FOLDER = 'templates'
-
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+imagesFolder = os.path.join('static','images')
+app.config['UPLOAD_FOLDER'] = imagesFolder
 
 from commons import get_detection, convertir_divisas
 
@@ -12,9 +12,13 @@ from commons import get_detection, convertir_divisas
 def predict():
 
 	if request.method=='GET':
-		return render_template('index.html')
+		img1 = os.path.join(app.config['UPLOAD_FOLDER'],'capturando.png')
+		return render_template('index.html',user_image=img1)
 
 	if request.method=='POST':
+		for uploaded_file in request.files.getlist('file'):
+			if uploaded_file.filename != '':
+				uploaded_file.save(uploaded_file.filename)
 		if 'file' not in request.files:
 			print('file not uploaded')
 			return
@@ -29,33 +33,6 @@ def predict():
 
 		cant_out = convertir_divisas(cant_total,div_in,div_out)
 		return render_template('result.html',result = result,cant_total = cant_total,div_out = div_out, cant_out = cant_out, div_in = div_in)
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
-
-@app.route('/show/flechaback.png')
-def uploaded_file(filename):
-    filename = 'http://127.0.0.1:5000/templates/' + filename
-    return render_template('result.html', filename=filename)
-
-@app.route('/uploads/flechaback.png')
-def send_file(filename):
-    return send_from_directory(UPLOAD_FOLDER, flechaback.png)
 
 
 if __name__ == '__main__':
