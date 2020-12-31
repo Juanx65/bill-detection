93ed2a8b8ff169bf5901740e5424f634fdbc103d
vyrkka <63375553+vyrkka@users.noreply.github.com>
2020-12-31 17:52:04 -0300

    cambio detect.py
    editado para solucionar cajas solapadas

--- RetinaNet/detect.py
+++ RetinaNet/detect.py
@@ -22,18 +22,24 @@ def get_session():
 
 tf.compat.v1.keras.backend.set_session(get_session)
 
-model_path = '/mnt/c/Users/juan_/Desktop/PDI_git/bill-detection/RetinaNet/models/inference_27.h5'   ## replace this with your model path
+model_path = 'inference_30.h5'   ## replace this with your model path
 #model_path = '/mnt/c/Users/ricar/Desktop'
 model = models.load_model(model_path, backbone_name='resnet50')
 
 labels_to_names = {0: '1kbill', 1:'2kbill', 2:'5kbill', 3:'10kbill',4: '20kbill'}                    ## replace with your model labels and its index value
 
-image_path = '/mnt/c/Users/juan_/Desktop/PDI_git/bill-detection/RetinaNet/images/v1.jpg'## replace with input image path
+image_path = 'prueba.jpeg'## replace with input image path
 #image_path = '/mnt/c/Users/ricar/Desktop/ale/juan_ssd.jpeg'  ## replace with input image path
 #output_path = 'C:\\Users\\Samjith.CP\\Desktop\\detected_image.jpg'   ## replace with output image path
 
 color_class =  {0 : [71,164,33], 1: [120,12,138], 2: [234,92,129], 3: [60,124,227],4:[250,119,43]}
 
+def solapadas(b1,b2):
+    if(abs(b1[0]-b2[0])<50 and abs(b1[1]-b2[1])<50):
+        return(True)
+    return(False)
+
+SCORE = 0.75
 def detection_on_image(image_path):
 
         image = cv2.imread(image_path)
@@ -43,19 +49,33 @@ def detection_on_image(image_path):
         image, scale = resize_image(image)
         boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))
         boxes /= scale
+        validos = []
         for box, score, label in zip(boxes[0], scores[0], labels[0]):
-
-            if score < 0.95:
-                break
+            if score < SCORE:
+                break                 
+            validos.append((box,score,label))
+        finales = []
+        comp = []
+        for box1, score1, label1 in validos:
+            agregar = True
+            comp = [k for k in finales]
+            for box2,score2,label2 in comp:
+                if(solapadas(box1,box2)):
+                    if(score2 >= score1):
+                        agregar = False
+                        break
+                    else:
+                        finales.remove((box2,score2,label2))
+            if(agregar):
+                finales.append((box1,score1,label1))
+        for box, score, label in finales:
 
             color = color_class[label]
             b = box.astype(int)
             x1,y1,x2,y2 = b
             box_w = x2 - x1
             box_h = y2 - y1
-
-            print(b)
-
+ 
             draw = cv2.rectangle(draw, (x1, y1 + box_h), (x2, y1), color, 5)
             draw_box(draw, b, color=color)
             cv2.putText(draw, labels_to_names[label] + "  "+  str(round(score,3)), (b[0],b[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 1.8, color, 5)
@@ -67,5 +87,5 @@ def detection_on_image(image_path):
         cv2.imwrite(name, detected_img)
 
         #cv2.imshow('Detection',detected_img)
-        #cv2.waitKey(0)
+        cv2.waitKey(0)
 detection_on_image(image_path)
