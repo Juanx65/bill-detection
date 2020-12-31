89313252e0c0023e127c165b37c1c1991fa30b87
Juanx65 <59270808+Juanx65@users.noreply.github.com>
2020-12-31 13:36:57 -0300

    Retinanet Update
    Se arreglaron las cajas englobantes con un formato vistoso, y organizacion en carpetas de los elementos.
    
    Co-Authored-By: RMardonesT <45024705+RMardonesT@users.noreply.github.com>

--- RetinaNet/detect.py
+++ RetinaNet/detect.py
@@ -9,6 +9,7 @@ import cv2
 import os
 import numpy as np
 import time
+import uuid
 
 import tensorflow as tf
 
@@ -21,16 +22,17 @@ def get_session():
 
 tf.compat.v1.keras.backend.set_session(get_session)
 
-model_path = '/mnt/c/Users/ricar/Desktop/PDI/bill-detection/RetinaNet/inference_Gpu.h5'   ## replace this with your model path
+model_path = '/mnt/c/Users/juan_/Desktop/PDI_git/bill-detection/RetinaNet/models/inference_27.h5'   ## replace this with your model path
+#model_path = '/mnt/c/Users/ricar/Desktop'
 model = models.load_model(model_path, backbone_name='resnet50')
 
 labels_to_names = {0: '1kbill', 1:'2kbill', 2:'5kbill', 3:'10kbill',4: '20kbill'}                    ## replace with your model labels and its index value
 
-image_path = '/mnt/c/Users/ricar/Desktop/ale/fotos/a5.png'  ## replace with input image path
+image_path = '/mnt/c/Users/juan_/Desktop/PDI_git/bill-detection/RetinaNet/images/v1.jpg'## replace with input image path
 #image_path = '/mnt/c/Users/ricar/Desktop/ale/juan_ssd.jpeg'  ## replace with input image path
 #output_path = 'C:\\Users\\Samjith.CP\\Desktop\\detected_image.jpg'   ## replace with output image path
 
-color_class =  {0 : [71,164,33], 1: [120,12,138], 2: [234,92,129], 3: [60,124,227], 5: [250,119,43]}
+color_class =  {0 : [71,164,33], 1: [120,12,138], 2: [234,92,129], 3: [60,124,227],4:[250,119,43]}
 
 def detection_on_image(image_path):
 
@@ -43,19 +45,27 @@ def detection_on_image(image_path):
         boxes /= scale
         for box, score, label in zip(boxes[0], scores[0], labels[0]):
 
-            if score < 0.65:
+            if score < 0.95:
                 break
 
             color = color_class[label]
             b = box.astype(int)
+            x1,y1,x2,y2 = b
+            box_w = x2 - x1
+            box_h = y2 - y1
+
             print(b)
 
-
+            draw = cv2.rectangle(draw, (x1, y1 + box_h), (x2, y1), color, 5)
             draw_box(draw, b, color=color)
-            caption = "{} {:.3f}".format(labels_to_names[label], score)
-            draw_caption(draw, b, caption)
+            cv2.putText(draw, labels_to_names[label] + "  "+  str(round(score,3)), (b[0],b[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 1.8, color, 5)
+
         detected_img =cv2.cvtColor(draw, cv2.COLOR_RGB2BGR)
-        cv2.imwrite("test_convert.jpg", detected_img)
+
+        image_name = "results_{}.jpg".format(uuid.uuid1())
+        name = "results/"+ image_name
+        cv2.imwrite(name, detected_img)
+
         #cv2.imshow('Detection',detected_img)
         #cv2.waitKey(0)
 detection_on_image(image_path)
